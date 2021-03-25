39c67ab3c249290232ad91e76b849eb865f90e41
Matthew Ward <mattrward1030@gmail.com>
2021-03-25 15:44:22 -0400

    added questions to the questions variable and added a badges js that can be exported to be used when the user selects one of them in the terminal

--- index.js
+++ index.js
@@ -2,7 +2,54 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'write a short description of your project',
+        name: "description",
+
+    },
+    {
+        type: 'input',
+        message: 'How to install your application?',
+        name: "installation",
+    },
+    {
+        type: 'input',
+        message: 'provide instructions and examples for use,
+        name: "usage",
+    },
+    {
+        type: 'list',
+        message: 'What license do you want to use'
+        name: 'license',
+        choices: ["MIT", "Mozilla Public License 2.0", "Apache 2.0", "Unlicense"]
+    },
+    {
+        type: 'input',
+        message: 'Names of other contributors',
+        name: "contributors",
+    },
+
+    {
+        type: 'input',
+        message: 'instructions on how to test application?',
+        name: "tests",
+    },
+    {
+        type: 'input',
+        message: 'Enter email that you can be reached for questions',
+        name: "questions",
+    },
+
+
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
---
+++ utils/badges.js
@@ -0,0 +1,8 @@
+const badges = {
+    "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    "Mozilla Public License 2.0": "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+    "Apache 2.0": "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    "Unlicense": "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+}
+
+module.exports = badges;
